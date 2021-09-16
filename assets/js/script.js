c50dde2e630ceb42a0800a15ac1e496667b395b5
Diogo Henrique <diogomike138@gmail.com>
2021-09-16 13:01:26 -0300

    wip: Criando validacao de email com js.

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function validateEmail(email) {
+    let re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+let inputEmail = document.querySelector('.email');
+let btnSubmit = document.querySelector('.submit');
+let span = document.querySelector('.span--valid');
+
+btnSubmit.addEventListener('click',(event)=> {
+    
+    if(!validateEmail(inputEmail.value)){
+        event.preventDefault();
+        inputEmail.classList.add('email--invalid')
+        span.classList.remove('span--valid')
+    }
+});
+
